0c7cd20fc52ca962422f7e137d6c8d937ea71ad6
Timo Sachsenberg <timo.sachsenberg@uni-tuebingen.de>
2023-03-23 09:32:09 +0100

    initial commit

---
+++ nuxl.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="UmetaFlow",
+    page_icon="resources/icon.png",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items=None,
+)
+st.markdown("### Hello World ###\n----------------------")
